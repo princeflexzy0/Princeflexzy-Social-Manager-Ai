c760c623c5bd7c8fd625af20f263294a37ecbd81
ATERE EMMANUEL OLUWASEYI <emmanuelatere44@gmail.com>
2026-05-12 14:29:31 +0000

    fix: point webhook to n8n

--- backend/utils/zapierTweet.js
+++ backend/utils/zapierTweet.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const logger = require('./logger');
 
-const ZAPIER_WEBHOOK = 'https://hook.eu1.make.com/okbyorjyitxw37oih16o5m9k9rre85sg';
+const ZAPIER_WEBHOOK = 'https://princeflexzy.app.n8n.cloud/webhook/a102283f-8fec-47f8-9fdd-413c46dcd23c';
 
 async function sendToZapier(text) {
   try {
